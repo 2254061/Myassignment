288dd7efc21f284233e4bdf0f45c9d68cd90a0a9
2254061 <jothi.perumal24@gmail.com>
2025-05-14 21:49:34 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,21 @@
+package homework.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version =91.0f;
+		String developer = "Google";
+		boolean Betatype =false;
+		int ReleaseYear = 2008;
+		char Shortcutkey ='C';
+		
+		System.out.println("Browser version :"+version+"\n"+
+		"Developer :"+developer+"\n"+"Beta Type :"+Betatype+"\n"+ 
+				"Release Year :"+ReleaseYear+"\n"+"Shortcut Key :"+Shortcutkey);
+		
+		
+	}
+
+}
