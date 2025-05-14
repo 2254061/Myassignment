59ba2bc528664301ee16549e33547971823fd503
2254061 <jothi.perumal24@gmail.com>
2025-05-14 21:48:12 +0530

    Add files via upload
    Day1

---
+++ Homework1.java
@@ -0,0 +1,19 @@
+package homework.day1;
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+	
+		int value =10;
+		
+		if (value >0) {
+			System.out.println("This is positive number");
+		}
+		else {
+			System.out.println("This is negative number");
+		}
+		
+		
+	}
+	
+}
